bb69b7b7eb4e344bc2bc4f966c579fa88cb6d35a
Rustam Mamedov <rustam.mamedov230@gmail.com>
2024-05-24 21:58:58 -0400

    Updates

---
+++ component---src-pages-404-js-2c0321dffc15b9e0b025.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_blog=self.webpackChunkmy_blog||[]).push([[883],{429:function(e,t,n){n.r(t),n.d(t,{Head:function(){return s}});var o=n(7294);n(4160);const a={color:"#232129",padding:"96px",fontFamily:"-apple-system, Roboto, sans-serif, serif"},l={marginTop:0,marginBottom:64,maxWidth:320};t.default=()=>o.createElement("main",{style:a},o.createElement("h1",{style:l},"Page not found"));const s=()=>o.createElement("title",null,"Not found")}}]);
+//# sourceMappingURL=component---src-pages-404-js-2c0321dffc15b9e0b025.js.map
